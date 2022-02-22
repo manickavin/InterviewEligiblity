491440da7b0bb7fbb8831f7360f753aa05c76cfc
manickavin <manickavin07@gmail.com>
2022-02-22 19:50:04 +0530

    successfully pushed

---
+++ src/com/accenture/Eligiblity.java
@@ -0,0 +1,37 @@
+package com.accenture;
+
+public class Eligiblity {
+
+	public static void main(String[] args) {
+		System.out.println("\t\tEmployee eligiblity criteria");
+		System.out.println("\t\t***********************");
+	
+		int passedOut= 2020;
+		int passedOut1= 2021;
+		System.out.println(passedOut + " and " + passedOut1 + " passed out students are eligible");
+		
+		float cgpa= 7.95f;
+		System.out.println("The required cgpa is: " + cgpa);
+		
+		int salary =25000;
+		System.out.println("The monthly salary is: "+ salary);
+		
+		byte arrear= 2;
+		System.out.println("No of arrears are allowed is: " + arrear );
+		
+		String b = "IT ";
+		String b1 = "CSE ";
+		String b2 = "ECE ";
+		System.out.println("Students from the following background must attend: " +b +b1 +b2 );
+		
+		char cab = 'y'; 
+		//y is yes
+		System.out.println("Does student from other backgroud can also attend: " + cab);
+		
+		System.out.println("\n\t\tThe following is the selected student details");
+		System.out.println("\t\t===============================================");
+		
+		StudentDetails d = new StudentDetails();
+		d.details();
+		}
+}
---
+++ src/com/accenture/StudentDetails.java
@@ -0,0 +1,29 @@
+package com.accenture;
+
+public class StudentDetails {
+	public void details() {
+
+		String name = "Manikkavethan";
+		byte age = 21;
+		char sex = 'M';
+		String dept = "IT";
+		float cgpa = 8.95f;
+		int year = 2021;
+		long phone = 9597056738l;
+		String email = "manickavin07@gmail.com";
+		boolean join = true;
+		
+		System.out.println("The student name is: " + name);
+		System.out.println("The student age is: " + age);
+		System.out.println("The student sex is: " +sex);
+		System.out.println("The student is from: " + dept + " department" );
+		System.out.println("The student cgpa is: "+cgpa);
+		System.out.println("The student is passed out from the year: "+year);
+		System.out.println("The student phone number is: "+phone);
+		System.out.println("The student email address is: "+email);
+		System.out.println("Is student is ready for immediate joining: "+ join );
+		
+		
+	}
+
+}
